8cebeb607130ef39150dc77fd7a6804280445465
kevin walton <kevinwalton@kevins-MacBook-Air.local>
2019-05-30 09:58:29 +0100

    Added functionality to remove & add 'show' class dynamically for content section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 // Grab all tab & tab content items to create node list (like an array - so can loop thru)
-const tabItems = document.querySelectorAll(".tab-item");
-const tabContentItems = document.querySelectorAll(".tab-content-item");
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-// Function to select tab content item (takes an event paramater)
+// Select tab content item
 function selectItem(e) {
- // Remove border from original tab before placing at new position
+ // Remove all show and border classes
  removeBorder();
- // Add border to current tab
- this.classList.add("tab-border");
+ removeShow();
+ // Add border to current tab item
+ this.classList.add('tab-border');
+ // Grab content item from DOM
+ const tabContentItem = document.querySelector(`#${this.id}-content`);
+ // Add show class
+ tabContentItem.classList.add('show');
 }
 
 // Remove border function
@@ -15,7 +20,10 @@ function removeBorder() {
  tabItems.forEach(item => item.classList.remove("tab-border"));
 }
 
+// Remove the 'show' class to change content displayed
+function removeShow() {
+ tabContentItems.forEach(item => item.classList.remove("show"));
+}
+
 // Loop through the nodelist(array) & listen for event
 tabItems.forEach(item => item.addEventListener("click", selectItem));
-
-console.log("Cunts");
